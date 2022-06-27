af5196f96bafdf37fb3c78959281b9f13a3686d6
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-27 13:46:04 +0300

    add images, the footer, and navigation, fetch data from api

--- Assets/index.js
+++ Assets/index.js
@@ -1,9 +1,37 @@
-fetch("https:www.themealdb.com/api/json/v1/1/search.php?s=Beef")
-  .then((data) => {
-      return data.json();
-  });
-  .then((completedata)=>{
-      console.log(completedata)
-  }
-  );
-  
+const searchButton = document.getElementById('searchBtn');
+const mealList = document.getElementById('meal');
+
+// event listener
+
+searchButton.addEventListener('click', getMealList);
+
+// get meal list that matches with ingredients
+function getMealList() {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if (data.meals) {
+            data.meals.forEach(meal => {
+                html += `<div class="mealItem" data-id = "${meal.idMeal}">
+                <div class="food-image">
+                    <img src= "${meal.strMealThumb}" width="300px">
+                </div>
+                <div class="food-name">
+                    <h3>${meal.strMeal}</h3>
+                    <a href="#" class="recipe-button">Get Recipie</a>
+                </div>
+            </div>`;
+            });
+            
+        }
+        else{
+            html = "Sorry, we couldn't find your meal"
+        }
+
+        mealList.innerHTML = html;
+    });
+    
+}
+   
