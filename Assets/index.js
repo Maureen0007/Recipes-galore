8fafd807c5f1a251f319576e80540f124f43fd6b
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-27 23:23:26 +0300

    submit final codde

--- Assets/index.js
+++ Assets/index.js
@@ -1,13 +1,13 @@
 const searchButton = document.getElementById('searchBtn');
 const mealList = document.getElementById('meal');
-const recipe = document.querySelector('recipes');
+const recipes = document.querySelector('recipe');
 const closeBtn = document.getElementById("close")
 const like = document.getElementById('likeBtn')
 
 // event listener
 
 searchButton.addEventListener('click', getMealList);
-mealList.addEventListener('click',getRecipe);
+mealList.addEventListener('click',getRecipes);
 
 // get meal list that matches with ingredients
 function getMealList() {
@@ -41,48 +41,39 @@ function getMealList() {
    
 
 
-function getRecipe(event) {
+function getRecipes(event) {
     event.preventDefault();
     if (event.target.classList.contains('recipe-button')) {
       let mealItem = event.target.parentElement.parentElement;
-      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i= ${mealItem.dataset.id}`)
      .then(response => response.json())
-     .then(data => foodRecipeModal(data.meals))
+     .then(data => mealRecipeModal(data.meals))
         
     }
     
 }
 
-function foodRecipeModal(meal) {
-    // console.log(meal);
-    // meal = meal[0];
-    let html = `
-    <button id="close">
-    <i class="fa-solid fa-circle-xmark"></i>
-    </button>
+// function mealRecipeModal(meal) {
+//     console.log(meal);
+//      meal = `meal ${meal.strMeal}`; 
+//     let html = "";
+//               html += `
+//                <h2 class= "recipe-title">${meal.strMeal}</h2>
+//                <p class = "recipe-category">${meal.strCategory}</p>
+//                <div claas ="rep-instructions">
+//                    <h3 id="instruction">Instructions</h3>
+//                    <p id="recipe-text">${meal.strInstructions}</p>
+//                </div>
+//                <div class="recipe-image">
+//                 <img src= "${meal.strMealThumb}" alt="" >
+//                 </div>
+//                 <div id="close">
+//                 </div>
+//                 <button id="likeBtn" class="like-button" onclick="Toggle()"><i class="fa-solid fa-heart" id="heart"></i></button> 
+//                 </div>`;
 
-    <div class="recipe-image">
-<img src="${meal.strMealThumb}" alt="" width="200px">
-</div>
-<h2 class= "recipe-title">${meal.strMeal}</h2>
-<div class="recipe-details">
-<h3 id="instruction">${meal.strCategory}</h3>
-<p id="recipe-text">${meal.instructions}</p>
-</div>
-<button id="likeBtn" class="like-button" onclick="Toggle()"><i class="fa-solid fa-heart" id="heart"></i></button>
+//     recipes.innerHTML = html;
+//     recipes.parentElement.classList.add('showRecipe')
+
     
-    `;
-    recipe.innerHTML = html;
-    recipe.parentElement.classList.add('showRecipe')
-    
-}
-function Toggle() {
-    if (like.style.color == "red") {
-        like.style.color = "white"
-        
-    }
-    else {
-        like.style.color = "red"
-    }  
-    
-}
+// }
